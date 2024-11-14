e4bb2c2286c34f041a20d22514c1f9ca514fd1e6
vantaicn <vantaicn.no1@gmail.com>
2024-11-14 20:55:16 +0700

    test husky

--- W6/backend/app.js
+++ W6/backend/app.js
@@ -43,10 +43,10 @@ hbs.registerHelper('floor', function (num) {
 });
 
 //Middleware
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+                    app.use(logger('dev'));
+                    app.use(express.json());
+                    app.use(express.urlencoded({ extended: true }));
+                    app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
