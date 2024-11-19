9c0ae2321d3635268147cc38331f580ffae77672
Phoegras <164360602+Phoegras@users.noreply.github.com>
2024-11-19 22:42:49 +0700

    Product feature (#6)
    * vercel
    
    * done filter, search, sort, pagination, relevant products
    
    ---------
    
    Co-authored-by: ltdthao <thao452004@gmail.com>

---
+++ express/controllers/indexController.js
@@ -0,0 +1,13 @@
+// controllers/homeController.js
+const Product = require('../models/Product');
+
+const getIndex = async (req, res) => {
+  try {
+    const products = await Product.find({}).limit(4).lean();
+    res.render('index', { layout: 'layout', title: 'Home', products });
+  } catch (error) {
+    res.status(500).send('Error fetching products');
+  }
+};
+
+module.exports = { getIndex };
