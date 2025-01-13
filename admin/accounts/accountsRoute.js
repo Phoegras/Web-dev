98ea62ab225db9fd63807ac1c0233751066f1202
Nguyen Van Tai <vantaicn.no1@gmail.com>
2025-01-13 09:52:40 +0700

    Accounts (#27)
    * account management
    
    * accounts management

--- admin/accounts/accountsRoute.js
+++ admin/accounts/accountsRoute.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const accountController = require('./accountsController');
 const authMiddleware = require('../middlewares/authMiddlewares');
 
-router.get('/user', accountController.getUserAccountList);
-router.get('/admin', accountController.getAdminAccountList);
+router.get('/:type', authMiddleware.isAuthenticated, accountController.getUserAccounts);
+router.get('/:type/api', authMiddleware.isAuthenticated, accountController.getUserAccountsApi);
+router.patch('/:type/:id/ban', authMiddleware.isAuthenticated, accountController.banAccount);
+router.patch('/:type/:id/unban', authMiddleware.isAuthenticated, accountController.unbanAccount);
 
 module.exports = router;
